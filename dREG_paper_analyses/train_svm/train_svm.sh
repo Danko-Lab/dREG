d7f24fdf2663361144fb62358e72924dd2ad9cd3
Charles <dankoc@gmail.com>
2014-11-18 14:12:47 -0500

    Added functions to compare GWAS SNPs in different classes of TREs.

--- dREG_paper_analyses/train_svm/train_svm.sh
+++ dREG_paper_analyses/train_svm/train_svm.sh
@@ -12,7 +12,7 @@
 STARTDIR=`pwd`
 
 ## Copy files to scratch space (/workdir and /SSD).
-SCRATCH=/SSD/cgd24/train_SVR
+SCRATCH=/SSD/cgd24.train_SVR/
 mkdir $SCRATCH
 cp /home/cgd24/projects/tss_detector/train_svm/train_svm.R $SCRATCH ## 
 cp /home/cgd24/projects/tss_detector/andre_hmm/hg19.k562.new_hmm2b.post2.bed $SCRATCH ## Andre's HMM Predictions
@@ -23,8 +23,8 @@ cp /home/cgd24/projects/tss_detector/data/k562/K562_unt.sort.bed.gz_*.bw $SCRATC
 cd $SCRATCH
 
 ## Intersect w/ DNase.
-zcat k562.dnase.UW.DUKE.inters.bed.gz > k562.dnase.UW.DUKE.inters.bed
-cat hg19.k562.new_hmm2b.post2.bed | awk 'BEGIN{OFS="\t"} {print $1,$2-100,$3+100}' | bedops --merge - | bedmap --echo --count gunzip k562.dnase.UW.DUKE.inters.bed - | grep -v "|0" | sed "s/|/\t/g" > hg19.k562.new_hmm2b.post2.dnase.bed
+gunzip k562.dnase.UW.DUKE.inters.bed.gz 
+cat hg19.k562.new_hmm2b.post2.bed | awk 'BEGIN{OFS="\t"} {print $1,$2-100,$3+100}' | bedops --merge - | bedmap --echo --count k562.dnase.UW.DUKE.inters.bed - | grep -v "|0" | sed "s/|/\t/g" > hg19.k562.new_hmm2b.post2.dnase.bed
 head hg19.k562.new_hmm2b.post2.dnase.bed
 
 
