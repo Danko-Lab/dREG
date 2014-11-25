ec2f172cc765b505630b7773924b2b25c36da0fb
Stephanie Hyland <steph.hyland@gmail.com>
2014-11-25 11:34:53 -0500

    allow for the input to be fractions and not millions

--- dREG_paper_analyses/validation/generic_subsample.py
+++ dREG_paper_analyses/validation/generic_subsample.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python
 # Given an input BED file, will subsample the lines down.
 # arguments are infile N1 N2 N3...
-# where each Ni is a number (in millions) of lines that we want in a subsampled file.
+# where each Ni is a number (in millions) of lines that we want in a subsampled file...
+# or a fraction of original output size!
 
 import random
 import gzip
@@ -13,17 +14,32 @@ if not '.bed' in infile_path:
     print 'ERROR: expecting a bed file.'
     sys.exit()
 
-num_lines = sum(1 for line in open(infile_path))
+# first, check what sort of input
 num_list = sys.argv[2:]
 subsamps = []
+# assume if all are <=1, it's a list of fractions
+# else, assume it's a list of read-depths in millions
 for num in num_list:
-    prob = float(num)*(1000000.0)/num_lines
-    print num
-    print prob
-    path = re.sub('.bed', str(num)+'m.bed', infile_path)
-    outfile = gzip.open(path,'w')
-    subsamps.append((prob,outfile))
+    if num > 1:
+        millions = True
+        break
+    millions = False
 
+if millions = True:
+    num_lines = sum(1 for line in open(infile_path))
+    for num in num_list:
+        prob = float(num)*(1000000.0)/num_lines
+        path = re.sub('.bed', str(num)+'m.bed', infile_path)
+        outfile = gzip.open(path, 'w')
+        subsamps.append((prob, outfile))
+else:
+    for num in num_list:
+        prob = num
+        path = re.sub('.bed', str(num)+'pc.bed', infile_path)
+        outfile = gzip.open(path, 'w')
+        subsamps.append((prob, outfile))
+
+# prepare to run through!
 i=0
 print 'Opening file', infile_path
 for line in gzip.open(infile_path):
