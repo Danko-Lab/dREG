b23bc3a54eda26d44c89655ce654913f583f2238
Stephanie Hyland <steph.hyland@gmail.com>
2014-11-25 11:47:57 -0500

    reports how it interprets the input

--- dREG_paper_analyses/validation/generic_subsample.py
+++ dREG_paper_analyses/validation/generic_subsample.py
@@ -29,14 +29,16 @@ num_lines = sum(1 for line in open(infile_path))
 print 'There are', num_lines,'lines in', infile_path+'!'
 
 if millions == True:
+    print 'Interpreting input as millions.'
     for num in num_list:
         prob = num*(1000000.0)/num_lines
-        path = re.sub('.bed', '_'+str(num)+'m.bed', infile_path)
+        path = re.sub('.bed', '_'+str(int(num))+'m.bed', infile_path)
         if not '.gz' in infile_path:
             path = path+'.gz'
         outfile = gzip.open(path, 'w')
         subsamps.append((prob, outfile))
 else:
+    print 'Interpreting input as fractions.'
     for num in num_list:
         prob = num
         path = re.sub('.bed', '_'+str(int(num*100))+'pc.bed', infile_path)
