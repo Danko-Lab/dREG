407e22c3cfe1f004b2c73a7c33be4a0b99f40612
Lauren Choate <lac334@cbsudanko.tc.cornell.edu>
2015-07-27 17:27:59 -0400

    raw read count function

--- dREG/src/read_genomic_data.h
+++ dREG/src/read_genomic_data.h
@@ -28,7 +28,7 @@ raw_data_t read_from_bigWig_r(const char *chrom, int start, int end, bigWig_t *b
 void get_genomic_data(int left_pos, int right_pos, zoom_params_t zoom, raw_data_t chrom_counts, genomic_data_point_t dp);
 void scale_genomic_data(zoom_params_t zoom, genomic_data_point_t dp);
 int max_dist_from_center(int n_sizes, int *window_sizes, int *half_n_windows);
-SEXP get_genomic_data_R(SEXP chrom_r, SEXP centers_r, SEXP bigwig_plus_file_r, SEXP bigwig_minus_file_r, SEXP model_r);
+SEXP get_genomic_data_R(SEXP chrom_r, SEXP centers_r, SEXP bigwig_plus_file_r, SEXP bigwig_minus_file_r, SEXP model_r, SEXP scale_r);
 
 #endif
 
