4750a4e36af39cec7ad907dc14fa1347f13dfc27
ZHONG WANG <tg826494@login3.stampede.tacc.utexas.edu>
2016-08-01 22:12:58 -0500

    Fix installation

--- rDeps.R
+++ rDeps.R
@@ -1,19 +1,20 @@
-install.packages("boot", repos = "http://cran.us.r-project.org")
-install.packages("grid", repos = "http://cran.us.r-project.org")
+#install.packages("boot", repos = "http://cran.us.r-project.org")
+#install.packages("grid", repos = "http://cran.us.r-project.org")
 install.packages("e1071", repos = "http://cran.us.r-project.org")
 install.packages("rphast", repos = "http://cran.us.r-project.org")
+install.packages("data.table", repos = "http://cran.us.r-project.org")
 install.packages("devtools", repos = "http://cran.us.r-project.org")
 library(devtools)
 # Here a bug in current devtools 1.12.0 if the installation starts from a sub-folder.
-if (class(try( install_github("andrelmartins/bigWig", subdir="bigWig", force=TRUE) ) )== "try-error")
+r <- try( install_github("andrelmartins/bigWig", subdir="bigWig", force=TRUE) )
+if ( class( r )== "try-error" )
 	stop("!!!!!Failed to install bigWig from github using install_github method.!!!!\n 
-	You can install manually as following commands\n
+	You can install manually using the following commands:\n
 	---------------------------------------------------\n
 	git clone https://github.com/andrelmartins/bigWig.git\n
-	cd bigWig
+	cd bigWig \n
 	R CMD INSTALL bigWig\n
-	---------------------------------------------------\n
-	");
+	---------------------------------------------------");
 
 
 
