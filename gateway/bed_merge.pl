bd874c722f21de6aa0c3b73224fce667ad2e0528
dREG Community User <dreg2016@br005.pvt.bridges.psc.edu>
2017-01-24 16:17:23 -0500

    gateway script on bridges.psc.edu

---
+++ gateway/bed_merge.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/perl
+#       Program outputs the column specificed from a TSV file (0 = first column ...)
+
+# Perl trim function to remove whitespace from the start and end of the string
+
+my $p_chrom;
+my $p_chromStart;
+my $p_chromEnd;
+
+$join= int($ARGV[0]);
+$doprint=0;
+
+while(<STDIN>) {
+  @SPL = split(/[\s\t]/);
+  $chrom= $SPL[0];
+  $chromStart=$SPL[1];
+  $chromEnd=$SPL[2];
+
+  if(($p_chrom eq $chrom) && (($p_chromEnd+$join)>=$chromStart)) {
+    $p_chromEnd=$chromEnd;
+  } else {
+    if($doprint == 1) {
+      print $p_chrom."\t".$p_chromStart."\t".$p_chromEnd."\n";
+    }
+    $p_chrom= $chrom;
+    $p_chromStart= $chromStart;
+    $p_chromEnd= $chromEnd;
+  }
+  $doprint = 1;
+}
+## Print the last entry.
+print $p_chrom."\t".$p_chromStart."\t".$p_chromEnd."\n";
+
+
