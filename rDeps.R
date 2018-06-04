d467201e99ce1a07381a10bbcbf610fe86ae1ce3
ZHONG WANG <wzhy2000@gmail.com>
2018-06-04 11:52:00 -0400

    Update rDeps.R

--- rDeps.R
+++ rDeps.R
@@ -1,11 +1,10 @@
-#install.packages("boot", repos = "http://cran.us.r-project.org")
-#install.packages("grid", repos = "http://cran.us.r-project.org")
 install.packages("snowfall", repos = "http://cran.us.r-project.org")
 install.packages("rmutil", repos = "http://cran.us.r-project.org")
 install.packages("mvtnorm", repos = "http://cran.us.r-project.org")
 install.packages("e1071", repos = "http://cran.us.r-project.org")
 install.packages("rphast", repos = "http://cran.us.r-project.org")
 install.packages("data.table", repos = "http://cran.us.r-project.org")
+install.packages("randomForest", repos = "http://cran.us.r-project.org")
 install.packages("devtools", repos = "http://cran.us.r-project.org")
 library(devtools)
 # Here a bug in current devtools 1.12.0 if the installation starts from a sub-folder.
