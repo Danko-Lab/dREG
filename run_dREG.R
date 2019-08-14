904c107ec95a20c17495fec34e877849bcc7fad1
Zhong Wang <wzhy2000@hotmail.com>
2019-08-14 10:11:38 -0400

    remove rdata

--- run_dREG.R
+++ run_dREG.R
@@ -1,6 +1,45 @@
 require(dREG)
 options("scipen"=100, "digits"=4)
 
+make_index_gz<-function( df_bed, out_file)
+{
+	file.tmp <- tempfile(fileext=".bed");
+	write.table( df_bed, file=file.tmp, row.names=FALSE, col.names=FALSE, quote=FALSE, sep="\t");
+	system(paste( "sort-bed ", file.tmp, " | bgzip -f > ", out_file, ".gz", sep="") );
+	system(paste( "tabix -f -p bed ", out_file, ".gz", sep="") );
+	unlink(file.tmp)
+}
+
+make_bw<-function( df_bed, out_file)
+{
+    ## decrease the size
+    df_bed[,4] <- round(df_bed[,4], 2);
+
+    file.tmp <- tempfile(fileext=".bed");
+    write.table( df_bed, file=file.tmp, row.names=FALSE, col.names=FALSE, quote=FALSE, sep="\t");
+
+    file.tmp2 <- tempfile(fileext=".bed");
+    system(paste( "sort-bed ", file.tmp, ">", file.tmp2) );
+    system(paste( "bedGraphToBigWig ", file.tmp2, out.chrom.info, out_file,  sep=" ") );
+
+    unlink(file.tmp)
+    unlink(file.tmp2)
+}
+
+make_chrom_info <- function( file.bw.plus, file.bw.minus, file.chrom.info)
+{
+    bw.plus <- load.bigWig(file.bw.plus);
+    bw.minus <- load.bigWig(file.bw.minus);
+
+    chrom <- rbind( cbind( bw.plus$chroms, bw.plus$chromSizes), cbind( bw.minus$chroms, bw.minus$chromSizes) );
+    chr.size <- unlist( lapply( unique(chrom[,1]), function(chr){max( as.numeric( chrom[which(chrom[,1]==chr),2])) } ) );
+
+    df.bed <- data.frame( V1=unique(chrom[,1]), V2=chr.size );
+    write.table( df.bed, file=file.chrom.info, row.names=FALSE, col.names=FALSE, quote=FALSE, sep="\t");
+}
+
+
+
 ## Process command arguments
 args <- commandArgs(trailingOnly=TRUE);
 
@@ -63,33 +102,35 @@ r$run.time = run.time;
 ## Now scan all positions in the genome ...
 cat("[", as.character(Sys.time()), "] 3) Saving the result to compressed bed files.\n");
 
-out.file0 <- paste(outfile, "dREG.raw.peak.bed", sep=".")
-out.file1 <- paste(outfile, "dREG.infp.bed", sep=".")
-out.file2 <- paste(outfile, "dREG.peak.full.bed", sep=".")
-out.file3 <- paste(outfile, "dREG.peak.score.bed", sep=".")
-out.file4 <- paste(outfile, "dREG.peak.broad.bed", sep=".")
-out.rdata <- paste(outfile, "dREG.rdata", sep=".")
+out.rdata<- paste(outfile, "dREG.rdata", sep=".")
+## RDATA file is used for debug
+## save(r, file=out.rdata );
 
-make_index_gz<-function( df_bed, out_file)
-{
-	file.tmp <- tempfile(fileext=".bed");
-	write.table( df_bed, file=file.tmp, row.names=FALSE, col.names=FALSE, quote=FALSE, sep="\t");
-	system(paste( "sort-bed ", file.tmp, " | bgzip -f > ", out_file, ".gz", sep="") );
-	system(paste( "tabix -f -p bed ", out_file, ".gz", sep="") );
-	unlink(file.tmp)
-}
+out.raw.peak.bed<- paste(outfile, "dREG.raw.peak.bed", sep=".")
+out.infp.bed <- paste(outfile, "dREG.infp.bed", sep=".")
+out.infp.bw  <- paste(outfile, "dREG.infp.bw", sep=".")
+out.score.bed<- paste(outfile, "dREG.peak.score.bed", sep=".")
+out.score.bw <- paste(outfile, "dREG.peak.score.bw", sep=".")
+out.prob.bed <- paste(outfile, "dREG.peak.prob.bed", sep=".")
+out.prob.bw  <- paste(outfile, "dREG.peak.prob.bw", sep=".")
+out.full.bed <- paste(outfile, "dREG.peak.full.bed", sep=".")
+out.chrom.info <- paste(outfile, "chrom.info", sep=".")
 
-save(r, file=out.rdata );
-
-#Rounding to 2 digits for predicted scores
 r$infp_bed[,4] <- round( r$infp_bed[,4], 3 );
 r$peak_bed[,4] <- round( r$peak_bed[,4], 3 );
 
-make_index_gz( r$raw_peak[,-2], out.file0 );
-make_index_gz( r$infp_bed, out.file1 );
-make_index_gz( r$peak_bed, out.file2 );
-make_index_gz( r$peak_bed[,c(1:4)], out.file3 );
-make_index_gz( r$peak_broad, out.file4 );
+make_chrom_info(ps_plus_path, ps_minus_path, out.chrom.info);
+make_index_gz( r$raw_peak[ r$raw_peak$prob != -1, ], out.raw.peak.bed);
+make_index_gz( r$infp_bed, out.infp.bed );
+make_index_gz( r$peak_bed, out.full.bed );
+make_index_gz( r$peak_bed[,c(1:4)], out.score.bed );
+
+r$peak_bed[,5] <- 1 - r$peak_bed[,5];
+make_index_gz( r$peak_bed[,c(1,2,3,5)], out.prob.bed );
+
+make_bw( r$infp_bed[,1:4], out.infp.bw );
+make_bw( r$peak_bed[,c(1:4)], out.score.bw );
+make_bw( r$peak_bed[,c(1,2,3,5)], out.prob.bw );
 
 #system( paste("tar -cvzf ", outfile, ".dREG.tar.gz", " ", outfile, ".dREG.*", sep="") );
 #cat("Result:", paste(outfile, ".dREG.tar.gz", sep=""), "\n");
